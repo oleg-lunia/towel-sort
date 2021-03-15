96c33839d6a001e0385913e33cb58e0a1828dc49
Oleg Lunia <oleglunja2@gmail.com>
2021-03-15 13:01:09 +0300

    refactoring: changed the file index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+      return [];
+  }
+  for (let i = 0; i < matrix.length ; i++) {
+      if ( i == 0 || (i % 2) == 0) {
+          matrix[i].sort((a,b) => a - b);
+      } else {
+          matrix[i].sort((a,b) => b - a);
+      }
+  }
+  return matrix.flat();
 }
